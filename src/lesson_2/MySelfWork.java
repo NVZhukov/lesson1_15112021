0a91bb065e0d56e9d4f333e06bf7f7e7c5ae720e
Zhukov Nikita <nypno@mail.ru>
2021-11-20 18:29:42 +0300

    Сделал второе домашнее задание, но без задания со звездочкой

---
+++ src/lesson_2/MySelfWork.java
@@ -0,0 +1,38 @@
+package lesson_2;
+
+public class MySelfWork {
+    public static void main(String[] args) {
+//        cheсkSum();
+//        cheсkNum();
+//        positiveOrNegativeNum(9);
+//        printText("Hello", 3);
+       isYear(2000);
+    }
+
+//    public static boolean cheсkSum(int a, int b) {
+//        int sum = a + b;
+//        return sum >= 10 && sum <= 20;
+//    }
+
+    //    public static void cheсkNum(int a){
+//        if(a<0){
+//            System.out.println(a+ " - отрицательное число");
+//        }else{
+//            System.out.println(a+ " - положительное число");
+//        }
+//    }
+//    public static boolean positiveOrNegativeNum(int a) {
+//        return a < 0;
+//    }
+
+//    public static void printText(String str, int a) {
+//        for (int i=0; i<a;i++) {
+//            System.out.println(str);
+//        }
+    public static void isYear(int year){
+
+    }
+}
+
+
+
