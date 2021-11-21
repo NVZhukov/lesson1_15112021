70ac7c0e02aaef959c117c6917b0634b453cb3a8
Zhukov Nikita <nypno@mail.ru>
2021-11-21 16:25:31 +0300

    сделал второе домашнее задание, правда без *

--- src/lesson_2/MySecondHomeWork.java
+++ src/lesson_2/MySecondHomeWork.java
@@ -2,45 +2,35 @@ package lesson_2;
 
 public class MySecondHomeWork {
     public static void main(String[] args) {
-//        cheсkSum();
-//        cheсkNum();
-//        positiveOrNegativeNum(9);
-//        printText("Hello", 3);
-        isLeapYear();
+        cheсkSum();
+        cheсkPositiveOrNegative();
+        isPositiveOrNegative();
+        printText("Привет",5);
+
+
     }
 
-//    public static boolean cheсkSum(int a, int b) {
-//        int sum = a + b;
-//        return sum >= 10 && sum <= 20;
-//    }
+    public static boolean cheсkSum(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
 
-    //    public static void cheсkNum(int a){
-//        if(a<0){
-//            System.out.println(a+ " - отрицательное число");
-//        }else{
-//            System.out.println(a+ " - положительное число");
-//        }
-//    }
-//    public static boolean positiveOrNegativeNum(int a) {
-//        return a < 0;
-//    }
+    public static void cheсkPositiveOrNegative(int a) {
+        if (a < 0) {
+            System.out.println(a + " - отрицательное число");
+        } else {
+            System.out.println(a + " - положительное число");
+        }
+    }
 
-//    public static void printText(String str, int a) {
-//        for (int i=0; i<a;i++) {
-//            System.out.println(str);
-//        }
+    public static boolean isPositiveOrNegative(int a) {
+        return a < 0;
+    }
 
-//    public static void isLeapYear(int year){
-//    if(year%4==0 && year%400==0){
-//        System.out.println("год високосный");
-//    } else if(year%100==0){
-//        System.out.println("год не високосный");
-//    }else {
-//        System.out.println("год високосный");
-//    }
-//    }
-        public static boolean isLeapYear(int year){
-
+    public static void printText(String str, int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(str);
+        }
     }
 }
 
